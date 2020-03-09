18cb6d2f5eda2a8816d6e9ec76c551e2b88a6c3d
Kevin Marciniak <kevin.marciniak@gmail.com>
2020-03-09 07:00:00 -0500

    Prime Number Refactor
    Redid the problems that used prime numbers. Going to put common functions into a package file - advanced_math.py.

--- problem0007.py
+++ problem0007.py
@@ -5,29 +5,7 @@
 # Code by Kevin Marciniak
 ###########################
 
-from math import *
-
-
-def is_prime(prime_test):
-    if prime_test <= 1:
-        return False
-
-    if prime_test == 2 or prime_test == 3:
-        return True
-    if prime_test % 2 == 0:
-        return False
-    if prime_test % 3 == 0:
-        return False
-
-    i = 5
-    sqrt_prime_test = sqrt(prime_test)
-    while i <= sqrt_prime_test:
-        if (prime_test % i) == 0 or (prime_test % (i + 2)) == 0:
-            return False
-        i += 6
-
-    return True
-
+from advanced_math import *
 
 # limit = 6
 limit = 10_001
